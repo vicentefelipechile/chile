047dd3eaf49b4892af5d2cc594a150911324ad54
vicentefelipechile <vicente@mapping-latam.cl>
2024-03-12 01:29:16 -0300

    commit

--- timelineGenerator.py
+++ timelineGenerator.py
@@ -2,7 +2,7 @@
 # ==== Timeline Generator ====
 # ============================
 
-
+from hashlib import md5
 import json
 
 
@@ -36,6 +36,7 @@ FOOTER: str = """
         var evento = contenido[key]
         items.add({
             id: key,
+            hash: evento.hash,
             content: evento.title,
             start: evento.date
         })
@@ -52,7 +53,7 @@ FOOTER: str = """
         // Cargar el contenido correspondiente
         if (evento) {
             argTitle.textContent = evento.title
-            argDescription.innerHTML = evento.description
+            argDescription.innerHTML = "<a href='https://vicentefelipechile.github.io/chile/#" + evento.hash + "'>Obtener enlace permanente</a><br><br>" + evento.description
 
             // Añadir enlaces para cada fuente encontrada
             argSources.innerHTML = ""
@@ -71,6 +72,33 @@ FOOTER: str = """
         }
     })
 
+
+    window.onload = () => {
+        const HashID = window.location.hash.substring(1)
+    
+        if (HashID) {
+            var evento = Object.values(contenido).find((evento) => { return evento.hash == HashID })
+
+            if (evento) {
+                argTitle.textContent = evento.title
+                argDescription.innerHTML = "<a href='https://vicentefelipechile.github.io/chile/#" + evento.hash + "'>Obtener enlace permanente</a><br><br>" + evento.description
+
+                // Añadir enlaces para cada fuente encontrada
+                argSources.innerHTML = ""
+                evento.source.forEach(function (fuente) {
+                    var link = document.createElement("a")
+                    link.href = fuente
+                    link.textContent = fuente
+                    link.target = "_blank"
+                    argSources.appendChild(link)
+                    argSources.appendChild(document.createElement("br"))
+                })
+
+                timeline.moveTo(evento.date)
+                timeline.setSelection([evento.id])
+            }
+        }
+    }
 })
 """
 
@@ -79,7 +107,7 @@ FOOTER: str = """
 # ============================
 
 # Estructura del json:
-# id: {title: "...", description: "...", sources: ["...", "..."], date: "..."}
+# [{title: "...", description: "...", sources: ["...", "..."], date: "..."}]
 
 # Obtener el archivo "cronograma.json"
 with open("cronograma.json", "r", encoding="utf-8") as file:
@@ -88,12 +116,16 @@ file.close()
 
 VariableGenerator: str = "    var contenido = {"
 
-for key, value in contenido.items():
+for key, value in enumerate(contenido):
     if value["date"] == "":
         print(f"El evento '{value['title']}' no tiene una fecha definida")
     
     if value["description"] == "":
         print(f"El evento '{value['title']}' no tiene una descripción definida")
+    
+    # Generar un hash para el identificador del evento
+    contenidoTotal: str = value["title"] + value["description"] + value["date"]
+    value["hash"] = md5(contenidoTotal.encode()).hexdigest()
 
     VariableGenerator += f"\n        {key}: {json.dumps(value, ensure_ascii=False)},"
 VariableGenerator = VariableGenerator[:-1]
